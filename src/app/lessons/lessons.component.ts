1a91e2cc5455d7ff82c86d7e5a6f7ac5eb2737da
Vy03 <vyorennity3904@gmail.com>
2024-07-11 15:50:52 +0700

    toggle bookmark

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -9,5 +9,18 @@ import { SidebarComponent } from "../sidebar/sidebar.component";
     imports: [SidebarComponent]
 })
 export class LessonsComponent {
+    bookmarkedLessons: string[] = [];
 
+    toggleBookmark(lessonCode: string) {
+        const index = this.bookmarkedLessons.indexOf(lessonCode);
+        if (index === -1) {
+            this.bookmarkedLessons.push(lessonCode);
+        } else {
+            this.bookmarkedLessons.splice(index, 1);
+        }
+    }
+
+    isBookmarked(lessonCode: string): boolean {
+        return this.bookmarkedLessons.includes(lessonCode);
+    }
 }
