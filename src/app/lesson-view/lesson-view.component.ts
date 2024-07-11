2b482d7e227ff5d06b0d1f31b15afb9ff82bce4f
Vy03 <vyorennity3904@gmail.com>
2024-07-11 19:29:37 +0700

    misc upd

--- src/app/lesson-view/lesson-view.component.ts
+++ src/app/lesson-view/lesson-view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lesson-view',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './lesson-view.component.css'
 })
 export class LessonViewComponent {
-
+  constructor(private router: Router) {}
+  
+  navigateToWebcam(){
+    this.router.navigate(['/webcam']);
+  }
 }
